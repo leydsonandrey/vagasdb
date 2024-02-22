7cfa432be90d32aaca9c04e518a1f44076688d18
Leydson Andrey <andrey-tar-xz@gmail.com>
2024-02-22 12:23:08 -0300

    refactor: database.js renomeado para db_business.js

--- src/db_business.js
+++ src/db_business.js
@@ -1,7 +1,7 @@
-const DATA_BASE = [
-  { title: "Atacadão", site: "https://www.atacadao.com.br/", trabalhe_conosco: "https://www.atacadao.com.br/institucional/trabalhe-conosco", pandape: "https://atacadao.pandape.infojobs.com.br/"},
+const DB_BUSINESS = [
+  { title: "Atacadão", site: "https://www.atacadao.com.br/", trabalhe_conosco: "https://www.atacadao.com.br/institucional/trabalhe-conosco", pandape: "https://atacadao.pandape.infojobs.com.br/" },
   { title: "Carrefour", site: "https://www.carrefour.com.br/", trabalhe_conosco: "https://www.carrefour.com.br/trabalhe-conosco", linkedin: "https://www.linkedin.com/company/carrefour-brasil/", portal_de_vagas: "https://vemsergrupocrf.com.br/", pandape: "https://grupocarrefourbrasil.pandape.infojobs.com.br/", infojobs: "https://www.infojobs.com.br/grupocarrefourbrasil" },
-  { title: "Hapvida", site: "https://www.hapvida.com.br/", gupy: "https://hapvidandi.gupy.io/"},
+  { title: "Hapvida", site: "https://www.hapvida.com.br/", gupy: "https://hapvidandi.gupy.io/" },
   { title: "Havan", site: "https://www.havan.com.br/", trabalhe_conosco: "https://portalcliente.havan.com.br/trabalhe-conosco" },
   { title: "Super Show", site: "https://redesupershow.com.br/", trabalhe_conosco: "https://redesupershow.com.br/trabalhe-conosco/" },
   { title: "Nordestão", site: "https://nordestao.com.br/", pandape: "https://nordestao.pandape.infojobs.com.br/" },
@@ -10,7 +10,7 @@ const DATA_BASE = [
   { title: "Carajás", site: "https://www.carajas.com.br/", trabalhe_conosco: "https://www.carajas.com.br/trabalheconosco", pandape: "https://carajas.pandape.infojobs.com.br/" },
   { title: "Queiroz Atacadão", site: "https://queirozatacadao.com.br/", email: "recrutamento@queirozatacadao.com.br", trabalhe_conosco: "https://queirozatacadao.com.br/trabalhe-conosco/" },
   { title: "Riachuelo", site: "https://www.riachuelo.com.br/", trabalhe_conosco: "https://www.riachuelo.com.br/carreiras/", gupy: "https://riachuelo.gupy.io/" },
-  { title: "Tim Brasil", site: "https://www.tim.com.br/", trabalhe_conosco: "https://www.tim.com.br/rn/sobre-a-tim/carreira",  gupy: "https://vemprotime.gupy.io/", linkedin: "https://www.linkedin.com/company/timbrasil/" },  
+  { title: "Tim Brasil", site: "https://www.tim.com.br/", trabalhe_conosco: "https://www.tim.com.br/rn/sobre-a-tim/carreira", gupy: "https://vemprotime.gupy.io/", linkedin: "https://www.linkedin.com/company/timbrasil/" },
 ]
 
-export { DATA_BASE }
+export { DB_BUSINESS }
