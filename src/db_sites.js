30aba3d9bcdaf23d0cca733f0e249c3a71f0651f
Leydson Andrey <andrey-tar-xz@gmail.com>
2024-02-22 12:23:49 -0300

    feat: Sites de Vagas database

---
+++ src/db_sites.js
@@ -0,0 +1,8 @@
+const DB_SITES = [
+  { title: "LinkedIn", site: "https://br.linkedin.com/jobs" },
+  { title: "Gupy", site: "https://portal.gupy.io/" },
+  { title: "InfoJobs", site: "https://www.infojobs.com.br/" },
+  { title: "Indeed", site: "https://br.indeed.com/" },
+]
+
+export { DB_SITES }
