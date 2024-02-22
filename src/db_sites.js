3bca944d31cd15a82a6c2a1ecdcba60280313b28
Leydson Andrey <leydsonandreydd@gmail.com>
2024-02-22 14:13:34 -0300

    update: sites database
    add: Vagas

--- src/db_sites.js
+++ src/db_sites.js
@@ -1,4 +1,5 @@
 const DB_SITES = [
+  { title: "Vagas", site: "https://www.vagas.com.br/" },
   { title: "LinkedIn", site: "https://br.linkedin.com/jobs" },
   { title: "Gupy", site: "https://portal.gupy.io/" },
   { title: "InfoJobs", site: "https://www.infojobs.com.br/" },
