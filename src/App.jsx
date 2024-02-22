5617959e66997678e1452f452242b3ec5af1f152
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-02-22 19:34:23 -0300

    refactor: adiciona a pasta components
    Move: Button and SearchBar to componentsfolder

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
-import { DB_BUSINESS } from "./db_business"
-import { DB_SITES } from "./db_sites"
+import { useState } from "react"
+import { DB_BUSINESS } from "./db/db_business"
+import { DB_SITES } from "./db/db_sites"
+import SearchBar from "./components/SearchBar"
+import Button from "./components/Button"
 
 function FilterableData({ data }) {
-  const [filterText, setFilterText] = useState('');
+  const [filterText, setFilterText] = useState("")
 
   return (
     <>
       <SearchBar
-        filterText={filterText}
-        onFilterTextChange={setFilterText}
+        value={filterText}
+        placeholder="Pesquisar..."
+        onChange={(e) => setFilterText(e.target.value)}
       />
-
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         <CardList
           data={data}
@@ -23,7 +25,7 @@ function FilterableData({ data }) {
 }
 
 function CardList({ data, filterText }) {
-  const cards = [];
+  const cards = []
 
   data.forEach((dataValue) => {
     if (
@@ -37,14 +39,14 @@ function CardList({ data, filterText }) {
       <Card
         data={dataValue}
         key={dataValue.title} />
-    );
-  });
+    )
+  })
 
   return (
     <>
       {cards}
     </>
-  );
+  )
 }
 
 function Card({ data }) {
@@ -62,54 +64,22 @@ function Card({ data }) {
   }
 
   return (
-    <>
-      <section className="border-style w-auto border rounded-xl text-neutral-50 shadow-style break-words p-3">
-        <div className="mb-2">
-          <h3 className="text-2xl">
-            <a className="link-hover text-neutral-50" href={data.site} target="_blank">{data.title}</a>
-          </h3>
-         {data.desc ? <h4 className=" text-neutral-400">{data.desc}</h4> : ""}
-          {data.email ? <h4><a className="hover:underline text-neutral-400" href={`mailto:${data.email}`}>{data.email}</a></h4> : ""}
-        </div>
-        <ul className="divide-y divide-neutral-800">
-          <ListLinks name="Trabalhe Conosco" data={data.trabalhe_conosco} />
-          <ListLinks name="Portal de Vagas" data={data.portal_de_vagas} />
-          <ListLinks name="LinkedIn" data={data.linkedin} />
-          <ListLinks name="Gupy" data={data.gupy} />
-          <ListLinks name="PandaPé" data={data.pandape} />
-          <ListLinks name="InfoJobs" data={data.infojobs} />
-        </ul>
-      </section >
-    </>
-  )
-}
-
-function SearchBar({
-  filterText,
-  onFilterTextChange,
-}) {
-  return (
-    <input
-      type="text"
-      value={filterText} placeholder="Pesquisar..."
-      onChange={(e) => onFilterTextChange(e.target.value)}
-      className="w-full h-10 mb-5 p-3 border-style rounded-md focus:border-1 focus:border-neutral-400 bg-neutral-950 focus:ring-4 focus:ring-neutral-700 focus:outline-0 text-neutral-50 placeholder-neutral-400 shadow-style"
-    />
-  );
-}
-
-function Button({ value, onClick, id, active }) {
-  let buttonProp = "p-2 border-style rounded-md mb-5"
-  return (
-    <input
-      type="button"
-      id={id}
-      value={value}
-      onClick={onClick}
-      className={active ?
-        `${buttonProp} text-neutral-950 bg-neutral-50 hover:bg-neutral-200` :
-        `${buttonProp} text-neutral-400 hover:text-neutral-50 hover:bg-neutral-900 shadow-style`}
-    />
+    <section className="border-style bg-neutral-900 w-auto border rounded-xl text-neutral-50 shadow-style break-words p-3">
+      <div className="mb-2">
+        <h3 className="text-2xl">
+          <a className="link-hover text-neutral-50" href={data.site} target="_blank">{data.title}</a>
+        </h3>
+        {data.email ? <h4><a className="hover:underline text-neutral-400" href={`mailto:${data.email}`}>{data.email}</a></h4> : ""}
+      </div>
+      <ul className="divide-y divide-neutral-800">
+        <ListLinks name="Trabalhe Conosco" data={data.trabalhe_conosco} />
+        <ListLinks name="Portal de Vagas" data={data.portal_de_vagas} />
+        <ListLinks name="LinkedIn" data={data.linkedin} />
+        <ListLinks name="Gupy" data={data.gupy} />
+        <ListLinks name="PandaPé" data={data.pandape} />
+        <ListLinks name="InfoJobs" data={data.infojobs} />
+      </ul>
+    </section>
   )
 }
 
