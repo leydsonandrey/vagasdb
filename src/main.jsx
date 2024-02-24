9dae81769f04d03a559499cb9a2e7ec387110957
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-02-24 20:56:30 -0300

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import Root from "./pages/root";
+import { FilterableData } from "./pages/root"
+import ErrorPage from "./error-page";
+
+import { DB_BUSINESS } from "./db/business"
+import { DB_SITES } from "./db/sites"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<FilterableData data={DB_BUSINESS} />} />
+      <Route path="sites-de-vagas" element={<FilterableData data={DB_SITES} />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
