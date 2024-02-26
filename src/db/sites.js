5aad9e2c0dfdb0683c8e5d990bd9b191d3ce1f0b
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-02-26 15:22:17 -0300

    feat: databases atualizados

--- src/db/sites.js
+++ src/db/sites.js
@@ -1,9 +1,17 @@
-const DB_SITES = [
-  { title: "Vagas", site: "https://www.vagas.com.br/" },
-  { title: "LinkedIn", site: "https://br.linkedin.com/jobs" },
-  { title: "Gupy", site: "https://portal.gupy.io/" },
-  { title: "InfoJobs", site: "https://www.infojobs.com.br/" },
-  { title: "Indeed", site: "https://br.indeed.com/" },
+export const DB_SITES = [
+  {
+    title: "Vagas", site: "https://www.vagas.com.br/"
+  },
+  {
+    title: "LinkedIn", site: "https://br.linkedin.com/jobs"
+  },
+  {
+    title: "Gupy", site: "https://portal.gupy.io/"
+  },
+  {
+    title: "InfoJobs", site: "https://www.infojobs.com.br/"
+  },
+  {
+    title: "Indeed", site: "https://br.indeed.com/"
+  },
 ]
-
-export { DB_SITES }
