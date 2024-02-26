a11aa920ab15f929d3835385cc2ef06dbaa07b15
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-02-26 15:22:17 -0300

    update: databases

---
+++ src/db/concursos.js
@@ -0,0 +1,5 @@
+export const DB_CONCURSOS = [
+  {
+    title: "Cesgranrio", site: "https://www.cesgranrio.org.br/concursos/principal.aspx"
+  },
+]
---
+++ src/db/cursos.js
@@ -0,0 +1,11 @@
+export const DB_CURSOS = [
+  {
+    title: "Fundação Bradesco", site: "https://www.ev.org.br/"
+  },
+  {
+    title: "Aprenda Mais (MEC)", site: "https://aprendamais.mec.gov.br/course/index.php"
+  },
+  {
+    title: "Coursera", site: "https://www.coursera.org/"
+  },
+]
