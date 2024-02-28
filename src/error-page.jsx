35378c4529f20164f3376339c3787666f2fe5acd
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-02-28 12:10:40 -0300

    refactor: mudanças pequenas

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -2,6 +2,7 @@ export default function ErrorPage() {
 
   return (
     <div className="text-neutral-50 text-center">
+      {/* TODO: Melhorar essa página */}
       <h1>Oops!</h1>
       <p>Página não encontrada!</p>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from "react-router-dom";
+} from "react-router-dom"
 
 import "./index.css";
 import Root from "./pages/root";
@@ -15,7 +13,6 @@ import ErrorPage from "./error-page";
 import { DB_EMPRESAS } from "./db/empresas"
 import { DB_SITES } from "./db/sites"
 import { DB_CURSOS } from "./db/cursos"
-import { DB_CONCURSOS } from "./db/concursos";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,10 +24,8 @@ const router = createBrowserRouter(
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
-);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
