a7e494a8223c5c1713e1d60fde964babf0483430
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-05-16 09:12:24 -0300

    feat: atualização da página sobre

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -7,9 +7,9 @@ export default function Sobre() {
     <>
       <div className="text-neutral-50">
         <h1>Um Lugar de Fácil Acesso</h1>
-        <p><strong>O <Link name={"Work With Us"} href={"https://workwithus.vercel.app"} /> foi criado para listar o “Trabalhe Conosco” em um lugar de fácil acesso</strong>, para que você não precise acessar diversos sites para achá-los. Atualmente, também listamos outros recursos que o ajudaram a encontrar um emprego, como a sessão de <strong>Cursos de Graça</strong>, onde é listado diversos sites que, como o nome diz, oferecem cursos com certificados gratuitamente.</p>
+        <p><strong>O <Link name={"Work With Us"} href={"https://workwithus.vercel.app"} /> foi criado para listar o “Trabalhe Conosco” em um lugar de fácil acesso</strong>, para que você não precise acessar diversos sites para achá-los. Além disso, listamos outros recursos que o auxiliaram a encontrar um emprego, como a sessão de Cursos de Graça, onde são listados diversos sites que oferecem cursos com certificados gratuitos.</p>
 
-        <p><strong>Gostaria que adicionássemos alguma empresa, site de curso, etc., ao site ou corrigíssemos alguma informação? Manda uma mensagem para o e-mail abaixo:</strong></p>
+          <p><strong>Gostaria que acrescentássemos uma empresa, site de curso, etc. ao site ou corrigíssemos alguma informação existente? Envie uma mensagem para o e-mail abaixo:</strong></p>
 
         <strong>
           <Link name={"andrey.tar.xz@gmail.com"} href={"mailto:andrey.tar.xz@gmail.com"} />
